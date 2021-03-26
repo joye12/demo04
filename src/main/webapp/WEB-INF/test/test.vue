64dd8e049c96b2ccb6ad021c83160fbc91a85f3b
joye12 <hzz17y@outlook.com>
2021-03-26 13:43:44 +0800

    3/26

---
+++ src/main/webapp/WEB-INF/test/test.vue
@@ -0,0 +1,27 @@
+<template>
+  $END$
+</template>
+
+<script>
+export default {
+  name: "test"
+}
+</script>
+
+
+<div id="app">
+   <p>{{ message }}</p>
+</div>
+
+<script>
+    export default{
+      el:"#app",
+      data: {
+        message:'hello，world'
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
